66be2ff2b39b18224df3caa60c4f219b1b2fe59d
Kushagra <kush12092001@gmail.com>
2020-08-20 14:02:18 +0530

    updated

--- Driver.py
+++ Driver.py
@@ -14,7 +14,7 @@ import os
 
 
 adam = Adam(0.0001)
-con = tf.keras.models.Sequential([
+model = tf.keras.models.Sequential([
                                     tf.keras.layers.Conv2D(16, (3,3), activation = 'relu', input_shape=(100, 100, 1)),
                                     tf.keras.layers.MaxPooling2D(2, 2),
                                  
@@ -44,10 +44,10 @@ con = tf.keras.models.Sequential([
                                     tf.keras.layers.Dense(1, activation='tanh')
 ])
 
-con.summary()
-con.compile(optimizer = Adam(lr = 0.0001), loss = 'mse', metrics = ['mae'])
+model.summary()
+model.compile(optimizer = Adam(lr = 0.0001), loss = 'mse', metrics = ['mae'])
 
-con.load_weights('./zLASTweights-0.0558.h5')
+model.load_weights('./z-1-weights-0.0521.h5')
 
 img_str = cv2.imread('s1.jpg',0)
 rows,cols = img_str.shape
@@ -56,18 +56,18 @@ smoothed_angle = 0
 
 
 i = 52123
-while(cv2.waitKey(10) != ord('q') and i<63825):
+while(cv2.waitKey(10) != ord('q') and i < 63825):
 	img1 = image.load_img("new_data/data/" + str(i) + ".jpg",color_mode='grayscale')
-	img1 = image.img_to_array(img1)/255.0
-	img = image.load_img("new_data/data/" + str(i) + ".jpg",color_mode='grayscale',target_size=[100,100])
-	img = image.img_to_array(img)/255.0
-	img_resh = np.reshape(img,[1,100,100,1])
+	img1 = image.img_to_array(img1) / 255.0
+	img = image.load_img("new_data/data/" + str(i) + ".jpg", color_mode = 'grayscale', target_size = [100, 100])
+	img = image.img_to_array(img) / 255.0
+	img_resh = np.reshape(img,[1, 100, 100, 1])
 	degrees = con.predict(img_resh) * 180.0 / scipy.pi
 	print("Predicted steering angle: " + str(degrees) + " degrees")
 	cv2.imshow("frame", cv2.cvtColor(img1, cv2.COLOR_RGB2BGR))
 	smoothed_angle += 0.2 * pow(abs((degrees - float(smoothed_angle))), 2.0 / 3.0) * (degrees - smoothed_angle) / abs(degrees - smoothed_angle)
-	M = cv2.getRotationMatrix2D((cols/2,rows/2),-float(smoothed_angle),1)
-	dst = cv2.warpAffine(img_str,M,(cols,rows))
+	M = cv2.getRotationMatrix2D((cols / 2, rows / 2), -float(smoothed_angle), 1)
+	dst = cv2.warpAffine(img_str, M, (cols, rows))
 	cv2.imshow("steering wheel", dst)
 	i += 1
 
